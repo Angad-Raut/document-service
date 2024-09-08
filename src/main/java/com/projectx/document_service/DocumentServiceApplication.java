8f3eaf9a4884a990725171d850f68e36dc43bf38
Angad-Raut <85058287+Angad-Raut@users.noreply.github.com>
2024-09-08 16:56:58 +0530

    changes commited

--- src/main/java/com/projectx/document_service/DocumentServiceApplication.java
+++ src/main/java/com/projectx/document_service/DocumentServiceApplication.java
@@ -3,16 +3,22 @@ package com.projectx.document_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-public class DocumentServiceApplication {
+public class DocumentServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentServiceApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DocumentServiceApplication.class);
+	}
 }
